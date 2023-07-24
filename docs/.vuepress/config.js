0c96ebbef9ce85f344599194eea57c137d794017
Jérémie LITZLER <2410556+JeremieLitzler@users.noreply.github.com>
2023-07-24 12:56:22 +0200

    Update config.js to generate search index

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,4 +15,11 @@ export default defineUserConfig({
   shouldPrefetch: false,
   bundler: viteBundler(),
   plugins: [],
+  onPrepared: async (app) => {
+    const searchIndex = SearchIndexBuilder.BuildIndex(pagesData);
+    //await app.writeTemp(
+    //  'searchIndex.js',
+    //  `export default ${JSON.stringify(searchIndex)}`,
+    //);
+  },
 });
