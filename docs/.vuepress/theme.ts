c21967ef54358e2abab9becdf298fcc534bea5d4
Jeremie Litzler <jeremie.litzler@gmail.com>
2023-07-25 08:13:58 +0200

    publish article bafang

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -1,4 +1,4 @@
-const version = '1.0.672';
+const version = '1.0.673';
 const publishDate = new Date(Date.now());
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
 const publishDateFormatted = new Intl.DateTimeFormat('fr-FR', {
